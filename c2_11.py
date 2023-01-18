78fb419909162155b7247c369088a9191ff38c48
Aswanikp33 <120626544+Aswanikp33@users.noreply.github.com>
2023-01-18 11:58:19 +0530

    Delete c2_11.py

--- c2_11.py
+++
@@ -1,9 +0,0 @@
-l=int(input("Enter length of rectangle:"))
-b=int(input("Enter the breadth of rectangle:"))
-h=int(input("Enter the height "))
-ar=lambda x,y:x*y
-asq=lambda x: x*x
-at=lambda x,y:0.5*x*y
-print("Area of rectangle:",ar(l,b))
-print("Area of square:",asq(l))
-print("Area of triangle:",at(b,h))
