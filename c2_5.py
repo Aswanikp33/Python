05bd1d9c6b8cd86fdb75a599627ce2dc70230f87
Aswanikp33 <120626544+Aswanikp33@users.noreply.github.com>
2023-01-18 11:58:51 +0530

    Delete c2_5.py

--- c2_5.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter a limit:"))
-for i in range(1,a+1):
-    for j in range(0,i+1):
-        a=i*j
-        if a==0:
-            continue
-        else:
-            print(a,end=" ")
-    print("\n")
